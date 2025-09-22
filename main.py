c8066b1de7708f9407b4e1265f968e3a292c4b33
Ben <benzwd59@gmail.com>
2025-09-22 15:13:05 +0200

    add config loader

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import os.path
-import sys
-
 from bacnet import BacnetServer
+from bacnet.config_loader import load_config
+
 
 def main():
     # if len(sys.argv) < 3:
     #   print(f"Usage : {os.path.basename(__file__)} -ip 0.0.0.0")
     #   sys.exit()
 
-    server = BacnetServer("MyDevice", 1000)
+    config = load_config()
+    device_config = dict(config["Device"])
+    server = BacnetServer(device_config["device_name"], device_config["device_identifier"])
     server.start()
 
 if __name__ == "__main__":
