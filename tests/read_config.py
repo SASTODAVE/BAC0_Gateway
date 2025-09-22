945ecb7db88354c12c573763ea35d32f94970715
Ben <benzwd59@gmail.com>
2025-09-22 15:37:22 +0200

    [TEST] add test for load device config

--- tests/read_config.py
+++ tests/read_config.py
@@ -1,9 +1,16 @@
 import unittest
 
+from bacnet.config_loader import load_config
+
 
 class ReadConfig(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_device_config(self):
+        config = load_config("config.ini")
+        device_config = dict(config["Device"])
+
+        self.assertIn("device_name", device_config)
+        self.assertIn("device_identifier", device_config)
+        self.assertIn("vendor_identifier", device_config)
 
 
 if __name__ == '__main__':
