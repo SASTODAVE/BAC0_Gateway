98f5f0b87000facf7a0417d2c716cef24c1b195a
Ben <benzwd59@gmail.com>
2025-09-25 13:42:52 +0200

    review config for add ip in config file

--- bacnet/config_loader.py
+++ bacnet/config_loader.py
@@ -1,10 +1,6 @@
-import configparser
+import yaml
 
-def load_config(path="config.ini"):
-    config = configparser.ConfigParser()
-    config.read(path)
+def load_config(path="config.yaml"):
+    with open(path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
     return config
-
-def load_client_config():
-    config = load_config()
-    return dict(config["Client"])
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import argparse
-
 from bacnet import BacnetServer, load_config
 from utils.helpers import check_ip
 from bacnet.api_client import fetch_objects
@@ -7,20 +5,21 @@ from bacnet.objects import create_objects_from_json
 
 def main():
     #Parser for ip
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-ip", "--ip", type=check_ip, required=True, help="Invalid ip address.")
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("-ip", "--ip", type=check_ip, required=True, help="Invalid ip address.")
 
-    args = parser.parse_args()
+    #args = parser.parse_args()
 
     #Load config
     config = load_config()
-    device_config = dict(config["Device"])
+    device_cfg = config.get("device", {})
 
     data = fetch_objects()
     create_objects_from_json(data)
+    check_ip(device_cfg["ip"])
 
     #Start server
-    server = BacnetServer(device_name=device_config["device_name"], device_id=device_config["device_identifier"], ip=args.ip)
+    server = BacnetServer(device_name=device_cfg.get("name"), device_id=device_cfg.get("identifier"), ip=device_cfg.get("ip"))
     server.start()
 
 if __name__ == "__main__":
